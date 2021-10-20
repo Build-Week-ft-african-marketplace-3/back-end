11c2d654d3bc8efcdf93d66018a658842693d999
Klove Adams <kloveadams@gmail.com>
2021-10-20 15:42:23 -0400

    added get to listings router

--- api/listings/listings-router.js
+++ api/listings/listings-router.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
+const Listings = require("./listings-model");
+// const {  } = require("./listings-middleware");
+// const restricted = require("../auth/auth-middleware");
+
+router.get("/", (req, res, next) => {
+  Listings.getAll()
+    .then((listing) => {
+      res.json(listing);
+    })
+    .catch(next);
+});
 
 module.exports = router;
